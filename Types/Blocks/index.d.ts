d91867089055473552ff9fc3981d4c03de8afaf3
Oleksii Surnin <bromkrololo@gmail.com>
2021-07-07 16:33:21 +0300

    Updated to version 117

--- Types/Blocks/index.d.ts
+++ Types/Blocks/index.d.ts
@@ -1,11 +1,22 @@
+/// <reference path="../Blocks.BlockAsset/index.d.ts" />
+/// <reference path="../Blocks.BlockAssets/index.d.ts" />
 /// <reference path="../Scene.BlockSceneRoot/index.d.ts" />
 declare class BlocksModule {
 /**
+* ```
+(get) assets: BlockAssets
+(set) (Not Available)
+```
+
+Get an object that allows access to the Block Assets.
+*/
+static readonly assets: BlockAssets;
+/**
 *  
- * instantiate(prototypeName: string, initialState: {[key: string]: any}): Promise<BlockSceneRoot>
+ * instantiate(blockAssetOrName: string | BlockAsset, initialState: {[key: string]: any}): Promise<BlockSceneRoot>
  *  
  * 
- * Instantiate a Block asynchronously from a prototype.
+ * Instantiate a Block asynchronously, given a Block Asset or its name.
  * When instantiating blocks, keep the following in mind:
  * - Creation of Blocks is bound to the same set of guidelines as `Scene.create()`.
  * - New blocks always get assigned a globally unique `identifier`.`
@@ -14,7 +25,7 @@ declare class BlocksModule {
  * - `name` in `initialState` is being used, unless it's not provided - then `dynamicBlock` is used.
  * - Block's inputs and outputs are not accessible until `SceneObjectBase.addChild()` call finishes
  */
-static instantiate(prototypeName: string, initialState: {[key: string]: any}): Promise<BlockSceneRoot>;
+static instantiate(blockAssetOrName: BlockAsset | string, initialState: {[key: string]: any}): Promise<BlockSceneRoot>;
 
 }
 export = BlocksModule;
---
+++ Types/BodyTracking.KeyPoint2D/index.d.ts
@@ -0,0 +1,22 @@
+/// <reference path="../Reactive.BoolSignal/index.d.ts" />
+/// <reference path="../Reactive.Point2DSignal/index.d.ts" />
+declare interface KeyPoint2D {
+/**
+* ```
+(get) isTracked: BoolSignal
+(set) (Not Available)
+```
+
+Boolean signal representing whether this body key point is being tracked right now or not.
+*/
+isTracked: BoolSignal;
+/**
+* ```
+(get) keyPoint: Point2DSignal
+(set) (Not Available)
+```
+
+Point signal representing the key point
+*/
+keyPoint: Point2DSignal;
+}
